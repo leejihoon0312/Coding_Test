0b7301debcb8f96b3f9c8f878240ded1fe040942
jihoon <wlgnsdl1016@naver.com>
2024-02-10 17:30:50 +0900

    프로그래머스 레벨 1

--- 프로그래머스/Lv. 1/숫자 짝꿍.py
+++ 프로그래머스/Lv. 1/숫자 짝꿍.py
@@ -1,23 +1,20 @@
-from collections import defaultdict
+from collections import Counter
 
 
 def solution(X, Y):
     answer = ""
 
-    x_dict = defaultdict(int)
-    y_dict = defaultdict(int)
+    common = dict(Counter(X) & Counter(Y))
 
-    for i in X:
-        x_dict[i] += 1
-    for i in Y:
-        y_dict[i] += 1
+    common = list(sorted(common.items(), key=lambda x: x[0], reverse=True))
 
-    for num in range(9, -1, -1):
-        if x_dict[str(num)] != 0 and y_dict[str(num)] != 0:
-            answer += str(num) * min(x_dict[str(num)], y_dict[str(num)])
+    for value, loop in common:
+        answer += (value * loop)
 
     if len(answer) == 0:
         return "-1"
+
     if answer[0] == "0":
         return "0"
+
     return answer
